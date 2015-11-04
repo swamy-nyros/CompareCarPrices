fa558cb903f59f798d92dc3eff7043c3d58c7004
swamy-nyros <swamy_nyros@yahoo.com>
2015-11-04 14:17:52 +0530

    master database integration

--- compare_cars_prices/compare_cars/models.py
+++ compare_cars_prices/compare_cars/models.py
@@ -4,19 +4,14 @@ from django.utils import timezone
 
 
 
-class CarDetails(models.Model):
+class Car_Master_Data(models.Model):
 	website_name = models.CharField(max_length=100,blank=True)
-	city = models.CharField(max_length=100,blank=True)
-	makes = models.CharField(max_length=100,blank=True)
-	car_model =models.CharField(max_length=100,blank=True)
-	price = models.IntegerField(blank=True)
-	model_year = models.IntegerField(blank=True)
-	car_title = models.CharField(max_length=500,blank=True)
-	car_href = models.URLField(max_length=5000,blank=True)
-	car_image = models.ImageField(max_length=500,blank=True)
-	car_color = models.CharField(max_length=50,blank=True)
-	created = models.DateTimeField(default=timezone.now)
-	updated = models.DateTimeField(auto_now=True)
+	city_id = models.CharField(max_length=500,blank=True)
+	city_name = models.CharField(max_length=500,blank=True)
+	make_id = models.CharField(max_length=500,blank=True)
+	make_name = models.CharField(max_length=500,blank=True)
+	model_id = models.CharField(max_length=500,blank=True)
+	model_name = models.CharField(max_length=500,blank=True)
 
 	def __str__(self):
 		return self.website_name
